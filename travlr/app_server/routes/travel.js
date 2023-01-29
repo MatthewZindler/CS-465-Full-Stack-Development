44cc46007f34a64809f13f1d4b09bc21f0a5e78f
MatthewZindler <82732925+MatthewZindler@users.noreply.github.com>
2023-01-28 19:33:44 -0600

    Added content for module 3
    Added handlebars for iterating on each trip information on the travel page. In addition I implemented the /travel routers that I incorrectly did last module.

---
+++ travlr/app_server/routes/travel.js
@@ -0,0 +1,8 @@
+/* routes travel.js*/
+const express = require('express');
+const router = express.Router();
+const controller= require('../controllers/travel');
+
+/* GET home page. */
+router.get('/', controller.travel);
+module.exports = router;
